3875982066623ef7ea879e5a1fde2f57cad7bf5c
Satyam1203 <nakuklbegana191170@gmail.com>
2021-10-15 02:45:53 +0530

    Register pages working

---
+++ client/src/container/Register/User.js
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+
+import "../Login/style.css";
+
+const weekDays = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+
+function Register({ auth }) {
+  const [formData, setFormData] = useState({});
+  const [status, setStatus] = useState("");
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setStatus("");
+
+    const workingDaysInputs = Array.from(
+      document.getElementsByName("working_days")
+    );
+    const working_days = workingDaysInputs.map((el) => el.value);
+
+    axios(`/api/user`, {
+      method: "POST",
+      data: formData,
+      "Content-Type": "application/json",
+    })
+      .then((res) => {
+        console.log(res.data);
+        setStatus(res.data.msg);
+      })
+      .catch(console.error);
+  };
+
+  if (auth) return <Redirect to="/" />;
+  return (
+    <>
+      <div className="form-wrapper" style={{ justifyContent: "flex-start" }}>
+        <h2>Register yourself</h2>
+        <form onSubmit={handleFormSubmit}>
+          <div className="input-wrapper">
+            <div>Name</div>
+            <input
+              type="text"
+              value={formData.name || ""}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <div>Phone</div>
+            <input
+              type="text"
+              value={formData.phone || ""}
+              onChange={(e) =>
+                setFormData({ ...formData, phone: e.target.value })
+              }
+              pattern="[0-9]{10}"
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <div>Email</div>
+            <input
+              type="email"
+              value={formData.email || ""}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}"
+              required
+            />
+          </div>
+          <div className="input-wrapper">
+            <div>Password</div>
+            <input
+              type="password"
+              value={formData.password || ""}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+        <p>{status}</p>
+      </div>
+    </>
+  );
+}
+
+export default Register;
