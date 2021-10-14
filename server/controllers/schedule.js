a88ebec53abf9348b7ab9e8b5f9654625fdbf47b
Satyam1203 <nakulbegana191170@gmail.com>
2021-10-14 20:35:48 +0530

    add required controller methods

--- server/controllers/schedule.js
+++ server/controllers/schedule.js
@@ -1,15 +1,9 @@
-const bcrypt = require("bcrypt");
 const User = require("../models/user");
-const authController = require("./auth");
 
 module.exports = {
   create: async (req, res) => {
     try {
-      const password = await bcrypt.hash(req.body.password, 10);
-      const user = await User.create({
-        ...req.body,
-        password,
-      });
+      const user = await User.create(req.body);
       res.json(user);
     } catch (e) {
       res.json({ error: e.message });
@@ -23,5 +17,4 @@ module.exports = {
       res.json({ error: e.message });
     }
   },
-  login: (req, res) => authController.login(req, res, User),
 };
---
+++ server/routes/index.js
@@ -0,0 +1,17 @@
+const router = require("express").Router();
+
+const userController = require("../controllers/user");
+const storeController = require("../controllers/store");
+const authController = require("../controllers/auth");
+
+router.route("/user").post(userController.create);
+router.route("/user/find").post(userController.get);
+router.route("/user/login").post(userController.login);
+
+router.route("/store").post(storeController.create);
+router.route("/store/find").post(storeController.get);
+router.route("/store/login").post(storeController.login);
+
+router.route("/logout").post(authController.logout);
+
+module.exports = router;
