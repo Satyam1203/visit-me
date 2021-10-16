20e73d08dda57d613099866e00f7ab7a5f8ea5bc
Satyam1203 <nakulbegana191170@gmail.com>
2021-10-16 16:38:42 +0530

    Add logo, favicon and properly size images

--- client/src/components/NavBar/index.js
+++ client/src/components/NavBar/index.js
@@ -3,16 +3,48 @@ import axios from "axios";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+import logo from "../../logo.svg";
 import { useAuth } from "../../App";
 
 const Title = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 12px 1rem;
-  padding-left: 2rem;
   align-items: center;
   background-color: darkslateblue;
   color: #fff;
+
+  h2 {
+    margin: 0;
+  }
+
+  .dropdown-select {
+    display: inline-block;
+    padding: 10px;
+    cursor: pointer;
+    .register-links {
+      display: none;
+      background: white;
+      position: absolute;
+      margin-top: 10px;
+      z-index: 1;
+      border: 1px solid grey;
+      border-radius: 0 12px 12px 12px;
+      width: max-content;
+      padding: 12px;
+      a {
+        background: darkslateblue;
+      }
+    }
+
+    &:hover,
+    &:active {
+      .register-links {
+        display: grid;
+        grid-gap: 2px;
+      }
+    }
+  }
 
   a {
     text-decoration: none;
@@ -21,18 +53,28 @@ const Title = styled.div`
     padding: 10px;
 
     &:hover {
-      color: #eee;
+      color: #ddd;
+    }
+  }
+
+  @media (max-width: 768px) {
+    h2 {
+      font-size: 20px;
+    }
+    .nav-links {
+      font-size: 14px;
     }
   }
 `;
 
 function Index() {
-  const { auth } = useAuth();
+  const { auth, setauth } = useAuth();
 
   const logout = () => {
     axios("/api/logout", { method: "POST" })
       .then((res) => {
         console.log(res.data);
+        setauth(res.authenticated);
       })
       .catch(console.error);
   };
@@ -40,6 +82,7 @@ function Index() {
   return (
     <Title>
       <h2>
+        <img src={logo} height="24" />
         <Link to="/">visit-me</Link>
       </h2>
       <div className="nav-links">
@@ -47,12 +90,16 @@ function Index() {
           <a onClick={logout}>Logout</a>
         ) : (
           <>
-            <Link to="/register/store">Store Registration</Link>
-            <Link to="/register/user">User Registeration</Link>
+            <div className="dropdown-select">
+              Register
+              <div className="register-links">
+                <Link to="/register/store">Store Registration</Link>
+                <Link to="/register/user">User Registeration</Link>
+              </div>
+            </div>
             <Link to="/login">Log In</Link>
           </>
         )}
-        <Link to="/">Home</Link>
       </div>
     </Title>
   );
